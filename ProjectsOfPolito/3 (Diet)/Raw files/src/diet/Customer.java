3547442efc0c45f8abdee600ae8c721b218cea3a
Mehran Bayat <157355713+mehraxn@users.noreply.github.com>
2025-11-30 10:21:13 +0100

    update

---
+++ ProjectsOfPolito/3 (Diet)/Raw files/src/diet/Customer.java
@@ -0,0 +1,29 @@
+package diet;
+
+
+public class Customer {
+	
+	public String getLastName() {
+		return null;
+	}
+	
+	public String getFirstName() {
+		return null;
+	}
+	
+	public String getEmail() {
+		return null;
+	}
+	
+	public String getPhone() {
+		return null;
+	}
+	
+	public void SetEmail(String email) {
+	}
+	
+	public void setPhone(String phone) {
+	}
+	
+	
+}
